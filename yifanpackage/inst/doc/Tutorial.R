633622df05f4ad7b053ac414860235432fce86a7
yifanwang87582024 <yisadfsa@umich.edu>
2024-11-20 02:07:22 -0500

    Delete yifanpackage directory

--- yifanpackage/inst/doc/Tutorial.R
+++
@@ -1,7 +0,0 @@
-## ----include = FALSE----------------------------------------------------------
-knitr::opts_chunk$set(echo = TRUE)
-library(yifanpackage)
-
-## -----------------------------------------------------------------------------
-linear_regression(mtcars, "mpg", c("wt", "hp", "qsec"))
-
